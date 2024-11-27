6afd9197fcd2381ee11065cedb762c6437eb5083
YS-RAPTOR <yashan.sumanaratne@gmail.com>
2024-11-27 18:21:09 +1100

    Remove V1

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://ys-raptor.github.io",
-	base: "/PortfolioV1",
+	base: "/Portfolio",
 	server: {
 		port: 8080,
 		host: true,
