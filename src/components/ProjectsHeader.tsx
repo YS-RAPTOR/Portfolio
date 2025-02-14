7eef4df0ef0f4ff71617565ddfadea51001e7d47
YS-RAPTOR <yashan.sumanaratne@gmail.com>
2025-02-14 21:47:05 +1100

    Changed project selector styling

--- src/components/ProjectsHeader.tsx
+++ src/components/ProjectsHeader.tsx
@@ -68,14 +68,9 @@ export const ProjectsHeader = () => {
             <h1 className="p-2 text-lg font-extralight md:p-4 lg:p-6 lg:text-2xl">
                 PROJECTS
             </h1>
-            <div
-                ref={ref}
-                className="flex flex-wrap items-end justify-center gap-px"
-            >
+            <div ref={ref} className="flex flex-wrap items-end justify-center">
                 <div
-                    className={
-                        "relative w-32 cursor-pointer bg-black text-center text-sm capitalize before:pointer-events-none before:absolute before:-inset-px before:-z-10 before:bg-zinc-50 before:content-[''] after:pointer-events-none after:absolute after:-top-px after:left-1/2 after:h-px after:w-lvw after:-translate-x-1/2 after:bg-zinc-50 after:content-['']"
-                    }
+                    className="relative w-32 cursor-pointer text-center text-sm capitalize"
                     onClick={() => {
                         if (noOfSelected(state) === state.length) {
                             updateState(
@@ -93,7 +88,7 @@ export const ProjectsHeader = () => {
                             "w-full transition-colors duration-200 ease-in-out " +
                             (noOfSelected(state) === state.length
                                 ? "bg-zinc-50 text-zinc-950 hover:bg-zinc-400 hover:text-zinc-50"
-                                : "bg-zinc-950 text-zinc-50 hover:bg-zinc-400 hover:text-zinc-950")
+                                : "bg-zinc-950 text-zinc-50 underline hover:bg-zinc-400 hover:text-zinc-950")
                         }
                     >
                         All
@@ -107,6 +102,7 @@ export const ProjectsHeader = () => {
                         onClick={() => {
                             toggleSelected(i);
                         }}
+                        last={i === jobTypes.length - 1}
                     />
                 ))}
             </div>
@@ -118,19 +114,18 @@ const Selector = (props: {
     prevState: SelectionState;
     state: SelectionState;
     onClick: () => void;
+    last: boolean;
 }) => {
     const stateClasses = props.state.selected
         ? `bg-${getJobColor(props.state.job)} hover:bg-zinc-400`
-        : `hover:bg-${getJobColor(props.state.job)}`;
+        : `hover:bg-${getJobColor(props.state.job)} underline`;
     const prevStateClasses = props.prevState.selected
         ? `bg-${getJobColor(props.prevState.job)}`
-        : "";
+        : "underline";
 
     return (
         <div
-            className={
-                "swap relative w-32 cursor-pointer bg-black text-center text-sm capitalize before:pointer-events-none before:absolute before:-inset-px before:-z-10 before:bg-zinc-50 before:content-[''] after:pointer-events-none after:absolute after:-top-px after:left-1/2 after:h-px after:w-lvw after:-translate-x-1/2 after:bg-zinc-50 after:content-['']"
-            }
+            className="swap relative w-32 cursor-pointer text-center text-sm capitalize"
             onClick={props.onClick}
         >
             <div className={`swap-top w-full ${prevStateClasses}`}>
