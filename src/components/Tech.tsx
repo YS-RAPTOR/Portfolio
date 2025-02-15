24b9d1565a3e7c06c0d95da1263dc20b5a52c66f
YS-RAPTOR <yashan.sumanaratne@gmail.com>
2025-02-15 18:45:13 +1100

    Basic styling of tech

--- src/components/Tech.tsx
+++ src/components/Tech.tsx
@@ -1,5 +1,7 @@
-export const TechName = () => {
-    return <div></div>;
+import { type Technology } from "../utils/lib.ts";
+
+export const TechName = (props: { tech: Technology }) => {
+    return <div className="border px-2 py-1">{props.tech}</div>;
 };
 
 export const TechWithLogo = () => {
