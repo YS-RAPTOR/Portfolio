51435d8d46b4d054a55da5bb6b1ef494ff7dcd58
YS-RAPTOR <yashan.sumanaratne@gmail.com>
2025-02-18 20:22:08 +1100

    scaffold of steps

--- src/components/FallingTech.tsx
+++ src/components/FallingTech.tsx
@@ -1,8 +1,22 @@
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
+import { gsap } from "gsap";
 import { technologies, getTechName } from "../utils/lib.ts";
+
+// TODO:
+// Spawn falling squares with collision.
+// Ability to move the collisions using mouse/pointer and dragging them.
+// Create a physics force that pushes out blocks when click on location.
+// Pick what I like.
+// Add text/SVG to the blocks
 
 export const FallingTech = () => {
     const ref = useRef<HTMLCanvasElement>(null);
+
+    useEffect(() => {
+        if (!ref.current) return;
+
+        return () => {};
+    }, []);
 
     return <canvas ref={ref} className="aspect-video w-full" />;
 };
