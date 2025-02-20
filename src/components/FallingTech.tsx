2cb5d430ade40c5916d40ac85334ad7369ff40b4
YS-RAPTOR <yashan.sumanaratne@gmail.com>
2025-02-20 18:47:01 +1100

    Loading svgs to canvas

--- src/components/FallingTech.tsx
+++ src/components/FallingTech.tsx
@@ -10,27 +10,33 @@ import RAPIER from "@dimforge/rapier2d-compat";
 import * as THREE from "three";
 
 const FallingConstants = {
+    BlockHeight: 0.125,
     VerticalSpacing: 0.2,
     VerticalStartLocation: 1.5,
     RotationRange: (60 * Math.PI) / 180,
     HorizontalRange: 0.5,
-    ImpulseForce: 0.05,
-    TorqueRange: 0.001,
+    ImpulseForce: 0.4,
     MaxDistance: 1,
+    FlipBounds: Math.PI / 2 + 0.1,
 };
 
 const randomColor = () => {
     return new THREE.Color(Math.random(), Math.random(), Math.random());
 };
 
+type SVG = SvgData & {
+    widthPx: number;
+    heightPx: number;
+};
+
 class Falling {
-    svgCanvas: HTMLCanvasElement;
-    svgs: SvgData[];
     renderer: THREE.WebGLRenderer;
     camera: THREE.OrthographicCamera;
     scene: THREE.Scene;
     world: RAPIER.World;
-    objects: { mesh: THREE.Mesh; collider: RAPIER.RigidBody }[] = [];
+    objects: { mesh: THREE.Mesh; collider: RAPIER.RigidBody; svg: SVG }[] = [];
+    width: number;
+    height: number;
 
     constructor(
         canvas: HTMLCanvasElement,
@@ -52,20 +58,24 @@ class Falling {
                 threshold: 0.8,
             },
         ).observe(canvas);
-        this.svgs = svgs;
-        this.svgCanvas = document.createElement("canvas");
 
         // Renderer
         this.camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0, 1);
 
-        this.renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
+        this.renderer = new THREE.WebGLRenderer({
+            canvas,
+            antialias: true,
+        });
         this.renderer.setSize(width, height);
-        this.renderer.setPixelRatio(window.devicePixelRatio);
+        this.width = width;
+        this.height = height;
+        this.renderer.setPixelRatio(window.devicePixelRatio * 1.5);
 
         this.scene = new THREE.Scene();
         this.scene.background = new THREE.Color(0x09090b);
 
         // Physics
+
         const gravity = { x: 0.0, y: -0.8 };
         this.world = new RAPIER.World(gravity);
 
@@ -97,46 +107,122 @@ class Falling {
                 Math.random() * FallingConstants.RotationRange * 2 -
                 FallingConstants.RotationRange;
 
-            this.spawn(this.svgs[i], { x, y }, rotation);
+            this.spawn(svgs[i], { x, y }, rotation);
+        }
+        for (let i = 0; i < svgs.length; i++) {
+            const y =
+                FallingConstants.VerticalStartLocation +
+                (i + svgs.length) * FallingConstants.VerticalSpacing;
+            const x =
+                Math.random() * FallingConstants.HorizontalRange * 2 -
+                FallingConstants.HorizontalRange;
+            const rotation =
+                Math.random() * FallingConstants.RotationRange * 2 -
+                FallingConstants.RotationRange;
+
+            this.spawn(svgs[i], { x, y }, rotation);
         }
     }
 
+    getSvgDim(svg: { width: number; height: number }) {
+        const heightThree = FallingConstants.BlockHeight;
+        const widthThree = heightThree * (svg.width / svg.height);
+
+        const maxDim = Math.min(this.width, this.height);
+
+        const widthPx = widthThree * maxDim * window.devicePixelRatio;
+        const heightPx = heightThree * maxDim * window.devicePixelRatio;
+
+        return {
+            widthPx,
+            heightPx,
+            widthThree,
+            heightThree,
+        };
+    }
+
+    svgToTexture(svg: SVG) {
+        return new Promise<THREE.Texture>((resolve, reject) => {
+            const img = new Image();
+
+            img.onload = () => {
+                const canvas = document.createElement("canvas");
+                canvas.width = svg.widthPx;
+                canvas.height = svg.heightPx;
+                const ctx = canvas.getContext("2d");
+                ctx?.drawImage(img, 0, 0, svg.widthPx, svg.heightPx);
+
+                const texture = new THREE.CanvasTexture(canvas);
+                texture.colorSpace = THREE.SRGBColorSpace;
+                texture.needsUpdate = true;
+                URL.revokeObjectURL(img.src);
+                resolve(texture);
+            };
+
+            img.onerror = (err) => {
+                URL.revokeObjectURL(img.src);
+                reject(err);
+            };
+
+            const svgBlob = new Blob([svg.svg], { type: "image/svg+xml" });
+            img.src = URL.createObjectURL(svgBlob);
+        });
+    }
+
     spawn(svg: SvgData, pos: { x: number; y: number }, rotation: number) {
-        // TODO: Load SVG to the blocks
-        // TODO: Calculate width and height based on SVG
+        // TODO: SVG updating texture when the size changes
 
-        console.log(svg);
+        const { widthPx, heightPx, widthThree, heightThree } =
+            this.getSvgDim(svg);
 
-        const width = 0.3;
-        const height = 0.1;
+        const newSvg: SVG = {
+            svg: svg.svg,
+            width: svg.width,
+            height: svg.height,
+            widthPx,
+            heightPx,
+        };
 
-        const geometry = new THREE.PlaneGeometry(width, height);
+        this.svgToTexture(newSvg)
+            .then((texture) => {
+                const geometry = new THREE.PlaneGeometry(
+                    widthThree,
+                    heightThree,
+                );
+                const material = new THREE.MeshBasicMaterial({
+                    map: texture,
+                    side: THREE.DoubleSide,
+                });
+                const mesh = new THREE.Mesh(geometry, material);
 
-        const material = new THREE.MeshBasicMaterial({ color: randomColor() });
-        const mesh = new THREE.Mesh(geometry, material);
-        mesh.position.set(pos.x, pos.y, 0);
-        mesh.rotation.set(0, 0, rotation);
-        this.scene.add(mesh);
+                mesh.position.set(pos.x, pos.y, 0);
+                mesh.rotation.set(0, 0, rotation);
+                this.scene.add(mesh);
 
-        const colliderDesc = RAPIER.ColliderDesc.cuboid(
-            width * 0.5,
-            height * 0.5,
-        );
-        const rigidBodyDesc = RAPIER.RigidBodyDesc.dynamic()
-            .setTranslation(pos.x, pos.y)
-            .setRotation(rotation);
-        const rigidBody = this.world.createRigidBody(rigidBodyDesc);
-        rigidBody.addTorque(
-            Math.random() * FallingConstants.TorqueRange * 2 -
-                FallingConstants.TorqueRange,
-            true,
-        );
+                const colliderDesc = RAPIER.ColliderDesc.cuboid(
+                    widthThree * 0.5,
+                    heightThree * 0.5,
+                );
+                const rigidBodyDesc = RAPIER.RigidBodyDesc.dynamic()
+                    .setTranslation(pos.x, pos.y)
+                    .setRotation(rotation);
+                const rigidBody = this.world.createRigidBody(rigidBodyDesc);
+                const collider = this.world.createCollider(
+                    colliderDesc,
+                    rigidBody,
+                );
+                collider.setRestitution(0.5);
+                collider.setDensity(1.0);
 
-        const collider = this.world.createCollider(colliderDesc, rigidBody);
-        collider.setRestitution(0.5);
-        collider.setDensity(1.0);
-
-        this.objects.push({ mesh, collider: rigidBody });
+                this.objects.push({
+                    mesh,
+                    collider: rigidBody,
+                    svg: newSvg,
+                });
+            })
+            .catch((err) => {
+                console.error("Failed to create Texture: ", err);
+            });
     }
 
     click(x: number, y: number) {
@@ -160,15 +246,33 @@ class Falling {
     }
 
     handleResize(width: number, height: number) {
+        this.width = width;
+        this.height = height;
         this.renderer.setSize(width, height);
+
+        for (let i = 0; i < this.objects.length; i++) {
+            const { widthPx, heightPx } = this.getSvgDim(this.objects[i].svg);
+            this.objects[i].svg.widthPx = widthPx;
+            this.objects[i].svg.heightPx = heightPx;
+        }
     }
 
     update() {
-        // TODO: Always upright
         for (let i = 0; i < this.objects.length; i++) {
             const { mesh, collider } = this.objects[i];
             const pos = collider.translation();
-            const rot = collider.rotation();
+            let rot = collider.rotation();
+            // Check if upright
+
+            if (
+                !(
+                    rot < FallingConstants.FlipBounds &&
+                    rot > -FallingConstants.FlipBounds
+                )
+            ) {
+                rot -= Math.PI;
+            }
+
             mesh.position.set(pos.x, pos.y, 0);
             mesh.rotation.set(0, 0, rot);
         }
@@ -180,14 +284,6 @@ class Falling {
         this.renderer.render(this.scene, this.camera);
         requestAnimationFrame(() => this.render());
     }
-
-    cleanup() {
-        if (this.svgCanvas.parentNode) {
-            this.svgCanvas.parentNode.removeChild(this.svgCanvas);
-        }
-        // @ts-ignore
-        this.svgCanvas = null;
-    }
 }
 
 export const FallingTech = (props: { svgs: SvgData[] }) => {
@@ -196,14 +292,21 @@ export const FallingTech = (props: { svgs: SvgData[] }) => {
 
     useEffect(() => {
         if (!ref.current || !container.current) return;
-        const falling = new Falling(
-            ref.current,
-            props.svgs,
-            container.current.clientWidth,
-            container.current.clientHeight,
-        );
+
+        let falling: Falling | undefined = undefined;
+
+        RAPIER.init().then(() => {
+            falling = new Falling(
+                ref.current!,
+                props.svgs,
+                container.current!.clientWidth,
+                container.current!.clientHeight,
+            );
+        });
 
         const handleResize = () => {
+            if (!falling) return;
+
             falling.handleResize(
                 container.current!.clientWidth,
                 container.current!.clientHeight,
@@ -211,6 +314,7 @@ export const FallingTech = (props: { svgs: SvgData[] }) => {
         };
 
         const handlePointerDown = (e: PointerEvent) => {
+            if (!falling) return;
             if (e.target === null) return;
             const target = e.target as HTMLElement;
 
@@ -226,7 +330,6 @@ export const FallingTech = (props: { svgs: SvgData[] }) => {
         return () => {
             window.removeEventListener("resize", handleResize);
             ref.current?.removeEventListener("pointerdown", handlePointerDown);
-            falling.cleanup();
         };
     }, []);
 
